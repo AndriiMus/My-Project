7fee609043bf5660588abd8db9dcefc20fd7ed78
MakeTSGreatAgain <andrii.mus00@gmail.com>
2021-09-29 17:25:09 +0300

    add new project

---
+++ 04-oard-sources/app.js
@@ -0,0 +1,36 @@
+const board = document.querySelector('#board');
+const colors = [];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+
+  square.addEventListener('mouseleave', () => removeColor(square));
+
+  board.append(square);
+}
+while (colors.length < 100) {
+    colors.push(`rgb(${rand(0, 255)}, ${rand(0, 255)}, ${rand(0, 255)})`);
+}
+
+// random number generator
+function rand(frm, to) {
+    return ~~(Math.random() * (to - frm)) + frm;}
+
+
+function setColor(element) {
+    const color = getRandomColor()
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+}
+
+function getRandomColor(){
+const index =  Math.floor(Math.random() *colors.length)
+return colors[index]
+}
